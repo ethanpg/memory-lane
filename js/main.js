da3eca1c87c13f6758775853a987586fb41928df
Ethan Phelps-Goodman <ethanpg@gmail.com>
2014-11-15 16:42:32 -0800

    can pan and zoom in second view; still can't follow links

--- js/main.js
+++ js/main.js
@@ -7,22 +7,57 @@ function initialize() {
       pitch: 0
     },
     visible: true,
-    imageDateControl: true,
-    addressControl: true
+    imageDateControl: true
   };
   window.panorama1 = new google.maps.StreetViewPanorama(document.getElementById('pano1'), panoramaOptions);
+
+  panoramaOptions.addressControl = false;
+  panoramaOptions.linksControl = false;
+  panoramaOptions.panControl = false;
+  panoramaOptions.zoomControl = false;
+
   window.panorama2 = new google.maps.StreetViewPanorama(document.getElementById('pano2'), panoramaOptions);
 
+  var eventLock = false;
+
   google.maps.event.addListener(panorama1, 'position_changed', function() {
-    panorama2.setPosition(panorama1.getPosition());
+    if (!eventLock) {
+      eventLock = true;
+      panorama2.setPosition(panorama1.getPosition());
+      eventLock = false;
+    }
   });
 
   google.maps.event.addListener(panorama1, 'pov_changed', function() {
-    panorama2.setPov(panorama1.getPov());
+    if (!eventLock) {
+      eventLock = true;
+      panorama2.setPov(panorama1.getPov());
+      eventLock = false;
+    }
   });
 
   google.maps.event.addListener(panorama1, 'zoom_changed', function() {
-    panorama2.setZoom(panorama1.getZoom());
+    if (!eventLock) {
+      eventLock = true;
+      panorama2.setZoom(panorama1.getZoom());
+      eventLock = false;
+    }
+  });
+
+  google.maps.event.addListener(panorama2, 'pov_changed', function() {
+    if (!eventLock) {
+      eventLock = true;
+      panorama1.setPov(panorama2.getPov());
+      eventLock = false;
+    }
+  });
+
+  google.maps.event.addListener(panorama2, 'zoom_changed', function() {
+    if (!eventLock) {
+      eventLock = true;
+      panorama1.setZoom(panorama2.getZoom());
+      eventLock = false;
+    }
   });
 
   $("[data-pano]").click(function() {
